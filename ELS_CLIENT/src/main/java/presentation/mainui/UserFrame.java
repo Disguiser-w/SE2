50d4a6468f90585d782de8a66733b00d1b72d065
18351886679 <m18351886679@163.com>
2015-10-29 09:20:18 +0800

    看看大小

---
+++ ELS_CLIENT/src/main/java/presentation/mainui/UserFrame.java
@@ -0,0 +1,20 @@
+package presentation.mainui;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * 所有Frame继承这个，
+ * */
+public class UserFrame {
+	private JPanel panel;
+	
+	private JLabel ImageLabel;
+	private JPanel messagePanel;
+	private JPanel functionPanel;
+	private JPanel operationPanel;
+	
+	public UserFrame(){
+		
+	}
+}
