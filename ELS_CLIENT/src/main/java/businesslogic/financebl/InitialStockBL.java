1ff8968c5075be0749a823a1cffb4523e5622987
wll14 <2457409219@qq.com>
2015-11-22 08:58:16 +0800

    本宝宝要去踢毽球了==

--- ELS_CLIENT/src/main/java/businesslogic/financebl/InitialStockBL.java
+++ ELS_CLIENT/src/main/java/businesslogic/financebl/InitialStockBL.java
@@ -3,14 +3,17 @@ package businesslogic.financebl;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import dataservice.businessdataservice.BusinessDataService;
+import dataservice.financedataservice.AccountDataService;
 import dataservice.financedataservice.InitialStockDataService;
+import dataservice.managedataservice.OrganizationDataService;
+import dataservice.repertorydataservice.RepertoryDataService;
 import dataservice.userdataservice.UserDataService;
 import businesslogicservice.financeblservice.InitialStockBLService;
 import po.AccountPO;
 import po.InitInfoPO;
 import po.OrganizationPO;
 import po.RepertoryPO;
-
 import po.UserPO;
 import po.VehiclePO;
 import vo.AccountVO;
@@ -25,6 +28,10 @@ public class InitialStockBL implements InitialStockBLService{
 	
 	private InitialStockDataService initData;
 	private UserDataService userData;
+	private OrganizationDataService organizationData;
+	private BusinessDataService businessData;
+	private RepertoryDataService repertoryData;
+	private AccountDataService accontData;
 	
 	
 	public InitialStockBL(){
@@ -45,8 +52,107 @@ public class InitialStockBL implements InitialStockBLService{
 		return initData.initInfo(po,time);
 	}
 
+	
 	/**
-	 * vo变po，存入持久化变量
+	 * 取出各个PO中的数据并po变vo，添加到InitInfoVO中
+	 * Q：这里取PO是通过每个DataService去调吗
+	 * */
+	public InitInfoVO poToVO(InitInfoPO po){
+		if(po==null)
+			return null;
+		
+//		ArrayList<UserPO> userPOs=po.getUserPOs();
+		ArrayList<UserPO> userPOs=userData.showAllUsers();
+		ArrayList<UserVO> userVOs;
+		if(userPOs==null){
+			userVOs=null;
+		}
+		else{
+			userVOs=new ArrayList<UserVO>();
+			for(UserPO p:userPOs){
+				UserVO vo=new UserVO(p.getName(), p.getID(), p.getPassword(), p.getProfession(), p.getOrganization(), p.getSalaryPlan(), p.getAuthority(), p.getGrades());
+				userVOs.add(vo);
+			}
+		}
+		
+//		ArrayList<OrganizationPO> organizationPOs=po.getOrganizationPOs();
+		ArrayList<OrganizationPO> organizationPOs=organizationData.getOrgnizationPOs();
+		ArrayList<OrganizationVO> organizationVOs;
+		if(organizationPOs==null){
+			organizationVOs=null;
+		}
+		else{
+			organizationVOs=new ArrayList<OrganizationVO>();
+			for(OrganizationPO p:organizationPOs){
+				OrganizationVO vo=new OrganizationVO(p.getCategory(), p.getOrganizationID(), p.getName(),p.getRepertory());
+				organizationVOs.add(vo);
+			}
+		}
+		
+//		ArrayList<VehiclePO> vehiclePOs=po.getVehiclePOs();
+		ArrayList<VehiclePO> vehiclePOs=businessData.getVehicleInfos();
+		ArrayList<VehicleVO> vehicleVOs;
+		if(vehiclePOs==null){
+			vehicleVOs=null;
+		}
+		else{
+		vehicleVOs=new ArrayList<VehicleVO>();
+		for(VehiclePO p:vehiclePOs){
+			VehicleVO vo=new VehicleVO(p.getID(), p.getEngineNumber(), p.getLicensePlateNumber(), p.getLowNumberPlate(), p.getBuyTime(), p.getServiceTime());
+			vehicleVOs.add(vo);
+		}
+		}
+		
+//		ArrayList<RepertoryPO> repertoryPOs=po.getRepertoryPOs();
+		ArrayList<RepertoryPO> repertoryPOs=repertoryData.getAllRepertory();
+		ArrayList<RepertoryVO> repertoryVOs;
+		if(repertoryPOs==null){
+			repertoryVOs=null;
+		}
+		else{
+			repertoryVOs=new ArrayList<RepertoryVO>();
+			for(RepertoryPO p:repertoryPOs){
+				RepertoryVO vo=new RepertoryVO(p.getRepertoryID(), p.getOwnerID(), p.getMaxRow(), p.getMaxShelf(), p.getMaxDigit(), p.getWarningRatio(), p.getStockNum(block));
+				repertoryVOs.add(vo);
+			}
+		}
+		
+		ArrayList<AccountPO> accountPOs=po.getAccountPOs();
+		ArrayList<AccountVO> accountVOs;
+		if(accountPOs==null){
+			accountVOs=null;
+		}
+		else{
+			accountVOs=new ArrayList<AccountVO>();
+			for(AccountPO p:accountPOs){
+				AccountVO vo=new AccountVO(p.getName(), p.getMoney());
+				accountVOs.add(vo);
+			}
+		}
+		InitInfoVO initInfoVO=new InitInfoVO(po.getDate(), userVOs, organizationVOs, vehicleVOs, repertoryVOs, accountVOs);
+		return initInfoVO;
+	}
+	
+	/**
+	 * pos变vos
+	 * */
+	public ArrayList<InitInfoVO> posToVOs(ArrayList<InitInfoPO> pos){
+		ArrayList<InitInfoVO> initInfoVOs;
+		if(pos==null){
+			initInfoVOs=null;
+		}
+		else{
+		initInfoVOs=new ArrayList<InitInfoVO>();
+		for(InitInfoPO p:pos){
+			InitInfoVO vo=poToVO(p);
+			initInfoVOs.add(vo);
+		}
+		}
+		return initInfoVOs;
+	}
+	
+	/**
+	 * 将已获取的vo变po，存入持久化变量
 	 * */
 	private InitInfoPO voToPO(InitInfoVO vo) {
 		// TODO Auto-generated method stub
@@ -62,7 +168,7 @@ public class InitialStockBL implements InitialStockBLService{
 		else{
 			userPOs=new ArrayList<UserPO>();
 			for(UserVO v:userVOs){
-				UserPO po=new UserPO(v.getName(), v.getID(), v.getPassword(), v.getProfession(), v.getOrganization(), v.getSalaryPlan(), v.getAuthority(), v.getGrades());
+				UserPO po=new UserPO(v.getName(), v.getID(), v.getProfession(), v.getOrganization(), v.getSalaryPlan(), v.getAuthority(), v.getGrades());
 				userPOs.add(po);
 			}
 		}
@@ -76,7 +182,7 @@ public class InitialStockBL implements InitialStockBLService{
 		else{
 			organizationPOs=new ArrayList<OrganizationPO>();
 			for(OrganizationVO v:organizationVOs){
-				OrganizationPO po=new OrganizationPO(category, organizationID, name);
+				OrganizationPO po=new OrganizationPO(v.getCategory(), v.getOrganizationID(), v.getName());
 				organizationPOs.add(po);
 			}
 		}
@@ -89,7 +195,7 @@ public class InitialStockBL implements InitialStockBLService{
 		else{
 			vehiclePOs=new ArrayList<VehiclePO>();
 			for(VehicleVO v:vehicleVOs){
-				VehiclePO po=new VehiclePO(ID, engineNumber, licensePlateNumber, lowNumberPlate, buyTime, serviceTime);
+				VehiclePO po=new VehiclePO(v.ID, v.engineNumber, v.licensePlateNumber, v.lowNumberPlate, v.buyTime, v.serviceTime);
 				vehiclePOs.add(po);
 			}
 		}
@@ -102,7 +208,7 @@ public class InitialStockBL implements InitialStockBLService{
 		else{
 			repInventoryPOs=new ArrayList<RepertoryPO>();
 			for(RepertoryVO v:repertoryVOs){
-				RepertoryPO po=new RepertoryPO(repertoryID, ownerID);
+				RepertoryPO po=new RepertoryPO(v.getRepertoryID(), v.getOwnerID(), v.getMaxRow(), v.getMaxShelf(), v.getMaxDigit(), v.getWarningRatio(), v.getStockNumArray());
 				repInventoryPOs.add(po);
 			}
 		}
@@ -140,100 +246,6 @@ public class InitialStockBL implements InitialStockBLService{
 			return pos;
 		}
 	
-	}
-	
-	
-	/**
-	 * po变vo,转化为显示
-	 * */
-	public InitInfoVO poToVO(InitInfoPO po){
-		if(po==null)
-			return null;
-		
-		ArrayList<UserPO> userPOs=po.getUserPOs();
-		ArrayList<UserVO> userVOs;
-		if(userPOs==null){
-			userVOs=null;
-		}
-		else{
-			userVOs=new ArrayList<UserVO>();
-			for(UserPO p:userPOs){
-				UserVO vo=new UserVO(p.getName(), p.getID(), p.getPassword(), p.getProfession(), p.getOrganization(), p.getSalaryPlan(), p.getAuthority(), p.getGrades());
-				userVOs.add(vo);
-			}
-		}
-		
-		ArrayList<OrganizationPO> organizationPOs=po.getOrganizationPOs();
-		ArrayList<OrganizationVO> organizationVOs;
-		if(organizationPOs==null){
-			organizationVOs=null;
-		}
-		else{
-			organizationVOs=new ArrayList<OrganizationVO>();
-			for(OrganizationPO p:organizationPOs){
-				OrganizationVO vo=new OrganizationVO(category, organizationID, name, repertory);
-				organizationVOs.add(vo);
-			}
-		}
-		
-		ArrayList<VehiclePO> vehiclePOs=po.getVehiclePOs();
-		ArrayList<VehicleVO> vehicleVOs;
-		if(vehiclePOs==null){
-			vehicleVOs=null;
-		}
-		else{
-		vehicleVOs=new ArrayList<VehicleVO>();
-		for(VehiclePO p:vehiclePOs){
-			VehicleVO vo=new VehicleVO(ID, engineNumber, licensePlateNumber, lowNumberPlate, buyTime, serviceTime);
-			vehicleVOs.add(vo);
-		}
-		}
-		
-		ArrayList<RepertoryPO> repertoryPOs=po.getRepertoryPOs();
-		ArrayList<RepertoryVO> repertoryVOs;
-		if(repertoryPOs==null){
-			repertoryVOs=null;
-		}
-		else{
-			repertoryVOs=new ArrayList<RepertoryVO>();
-			for(RepertoryPO p:repertoryPOs){
-				RepertoryVO vo=new RepertoryVO(p.getRepertoryID(), p.getOwnerID(), p.getMaxRow(), p.getMaxShelf(), p.getMaxDigit(), p.getWarningRatio());
-				repertoryVOs.add(vo);
-			}
-		}
-		
-		ArrayList<AccountPO> accountPOs=po.getAccountPOs();
-		ArrayList<AccountVO> accountVOs;
-		if(accountPOs==null){
-			accountVOs=null;
-		}
-		else{
-			accountVOs=new ArrayList<AccountVO>();
-			for(AccountPO p:accountPOs){
-				AccountVO vo=new AccountVO(p.getName(), p.getMoney());
-				accountVOs.add(vo);
-			}
-		}
-		InitInfoVO initInfoVO=new InitInfoVO(po.getDate(), userVOs, organizationVOs, vehicleVOs, repertoryVOs, accountVOs);
-		return initInfoVO;
-	}
-	
-	/**
-	 * pos变vos
-	 * */
-	public ArrayList<InitInfoVO> posToVOs(ArrayList<InitInfoPO> pos){
-		ArrayList<InitInfoVO> initInfoVOs;
-		if(pos==null){
-			initInfoVOs=null;
-		}
-		else{
-		initInfoVOs=new ArrayList<InitInfoVO>();
-		for(InitInfoPO p:pos){
-			InitInfoVO vo=poToVO(p);
-			initInfoVOs.add(vo);
-		}
-		}
-		return initInfoVOs;
 	}
 
 	
