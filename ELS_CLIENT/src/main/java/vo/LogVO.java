10e9b3dfbd5bfc6125c2c24651ecb9cc87de4c2d
heyLizi <weiyanshuwys@126.com>
2015-11-28 16:58:26 +0800

    这样登录可以咩???

---
+++ ELS_CLIENT/src/main/java/vo/LogVO.java
@@ -0,0 +1,13 @@
+package vo;
+
+public class LogVO {
+
+	private String logReply;
+	private UserVO uservo;
+	
+	public LogVO(String logReply, UserVO uservo){
+		this.logReply = logReply;
+		this.uservo = uservo;
+	}
+	
+}
