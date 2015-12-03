715db281381ccac850017dfe5f0dd75b1e6cd6e7
tinymooooo <vigoss_zh@163.com>
2015-12-03 09:24:52 +0800

    data factory

--- ELS_CLIENT/src/main/java/businesslogic/datafactory/DataFactory.java
+++ ELS_CLIENT/src/main/java/businesslogic/datafactory/DataFactory.java
@@ -7,18 +7,27 @@ import java.rmi.RemoteException;
 
 import dataservice.businessdataservice.BusinessDataService;
 import dataservice.expressdataservice.ExpressDataService;
-
+import dataservice.intermediatedataservice.IntermediateDataService;
 
 //暂时使用 localhost:8888
 public class DataFactory {
-	public static ExpressDataService getExpressData() throws MalformedURLException, RemoteException, NotBoundException {
-		ExpressDataService expressData = (ExpressDataService) Naming.lookup("//localhost:8888/ExpressDataService");
+	public static ExpressDataService getExpressData()
+			throws MalformedURLException, RemoteException, NotBoundException {
+		ExpressDataService expressData = (ExpressDataService) Naming
+				.lookup("//localhost:8888/ExpressDataService");
 		return expressData;
 	}
 
 	public static BusinessDataService getBusinessData()
 			throws MalformedURLException, RemoteException, NotBoundException {
-		BusinessDataService businessData = (BusinessDataService) Naming.lookup("//localhost:8888/BusinessDataService");
+		BusinessDataService businessData = (BusinessDataService) Naming
+				.lookup("//localhost:8888/BusinessDataService");
 		return businessData;
+	}
+
+	public static IntermediateDataService getIntermediateDate()
+			throws MalformedURLException, RemoteException, NotBoundException {
+        IntermediateDataService intermediateData = (IntermediateDataService) Naming.lookup("//localhost:8888/IntermediateDataService")
+	    return intermediateData;
 	}
 }
