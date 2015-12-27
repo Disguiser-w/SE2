a7719c602341ae390fa24ddc54ca817ac65fa97a
18351886679 <m18351886679@163.com>
2015-12-27 14:24:56 +0800

    重影

--- ELS_CLIENT/src/main/java/presentation/commonui/OperationPanel.java
+++ ELS_CLIENT/src/main/java/presentation/commonui/OperationPanel.java
@@ -46,7 +46,7 @@ public class OperationPanel extends JPanel implements Observer {
 	}
 
 	public void paintComponent(Graphics g) {
-		// super.paintComponent(g);
+		 super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
@@ -57,6 +57,7 @@ public class OperationPanel extends JPanel implements Observer {
 			g2d.fillRect(0, 0, getWidth(), getHeight());
 		} else if (UserFrame.type == UserFrame.TYPE_1)
 			g2d.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+	
 	}
 
 
